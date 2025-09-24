2f500f3b15a1d9fa2920f52086f783552e9cc0cd
iamseop <165784018+iamseop@users.noreply.github.com>
2025-09-24 15:22:08 +0900

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="w-full max-w-3xl mx-auto mt-12 py-6 text-center text-gray-400 text-sm border-t border-border">
-      <p className="mb-2">Made by <A href = "https://abel.io.kr/" target = "blank" className="text-gray-400 hover:underline"> abel.io.kr </A></p>
+      <p className="mb-2">Made by <a href = "https://abel.io.kr/" target = "blank" className="text-gray-400 hover:underline"> abel.io.kr </a></p>
       <p>사이트 문제나 건의사항은 <a href="mailto:lucky_77_@naver.com" className="text-gray-400 hover:underline">lucky_77_@naver.com</a> 로 연락 주세요.</p>
     </footer>
   );
